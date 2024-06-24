f1c4ccc4b0677afe1a29c6784b7c134d4ad7c05f
Admin <shivendrawk@gmail.com>
2024-06-24 14:10:34 +0530

    second

--- src/BasicMarhs.java
+++ src/BasicMarhs.java
@@ -26,19 +26,88 @@ class BasicMarhs {
 //        System.out.println(reverse);
 
         //palindrom checher
-        int originalValue = n;
-        int reverse =0;
-        while(n>0){
-            int ld = n % 10;
-            reverse = (reverse * 10) + ld;
-            n /= 10;
+//        int originalValue = n;
+//        int reverse =0;
+//        while(n>0){
+//            int ld = n % 10;
+//            reverse = (reverse * 10) + ld;
+//            n /= 10;
+//        }
+//        if(reverse == originalValue){
+//            System.out.println("Yes a Paindrom number");
+//        }else{
+//            System.out.println("Not a palindrom");
+//        }
+
+        //Armstrong number
+//        int temp2 = n;
+//        int original = n;
+//        int counter = 0;
+//        int rem=0;
+//        int arm= 0;
+//        int mul =1;
+//        while(n>0){
+//            n /= 10;
+//            counter++;
+//        }
+//        while(temp2 !=0){
+//            mul =1;
+//            rem = temp2 % 10;
+//            for(int i = 1; i<=counter; i++){
+//                mul = mul*rem;
+//            }
+//            temp2 /= 10;
+//            arm += mul;
+//        }
+//        if(arm == original){
+////            System.out.println(arm);
+//            System.out.println("Yes an armstrong number");
+//        }else{
+////            System.out.println(arm);
+//            System.out.println("Not an Armstrong");
+//        }
+
+        //divisors
+//        for(int i = 1; i<=n; i++){
+//            if(n%i==0){
+//                System.out.print(i+" ");
+//            }
+//        }
+       // optimized
+//        for(int i =1; i*i<=n; i++){
+//            if(n%i == 0){
+//                System.out.println(i);
+//                if(n/i != i){
+//                    System.out.println(n/i);
+//                }
+//            }
+//        }
+
+        // CHECK PRIME NUMBER
+        int count =0;
+        for (int i=1; i*i<=n; i++){
+            if(n%i == 0){
+                count++;
+                if(n/i != i){
+                    count++;
+                }
+            }
         }
-        if(reverse == originalValue){
-            System.out.println("Yes a Paindrom number");
+        if(count == 2){
+            System.out.println("yes prime");
         }else{
-            System.out.println("Not a palindrom");
+            System.out.println("Not a prime");
         }
-//        System.out.println(reverse);
+
+
+        //sum of all divisors
+//        int sum =0;
+//        for(int i = 1; i<=n; i++){
+//            if(n%i==0){
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);;
     }
 }
 
